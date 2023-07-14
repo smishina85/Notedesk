3cb7c50fe507f10dfb9a020d92428be02de372ab
Svetlana Mishina <snys@mac.com>
2023-07-14 12:07:12 +0300

    Updated version: Menu, notifications, apscheduler, reply acception

--- notedesk_project/notedesk_project/urls.py
+++ notedesk_project/notedesk_project/urls.py
@@ -24,8 +24,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
-    # path('accounts/', include('django.contrib.auth.urls')),
-    # path('accounts/', include('accounts.urls')),
     path('accounts/', include("allauth.urls")),
     path('', include('notes.urls')),
 ]
